c162d8a99e623cd64a7c2e784a826c327ff83768
Frank Wallis <fwallis@outlook.com>
2015-03-23 19:46:17 +0000

    fix(references): add tests for ambient references

--- test/compiler-compile-spec.js
+++ test/compiler-compile-spec.js
@@ -7,7 +7,7 @@ var Traceur = require('traceur');
 
 // Traceur will compile all JS aside from node modules
 Traceur.require.makeDefault(function(filename) {
-  return !(/node_modules/.test(filename));
+   return !(/node_modules/.test(filename));
 });
 
 var Compiler = require('../lib/incremental-compiler').IncrementalCompiler;
@@ -15,6 +15,7 @@ var formatErrors = require('../lib/format-errors').formatErrors;
 
 var missingFile = '/somefolder/fixtures/program1/missing-file.ts';
 var missingImport = require.resolve('./fixtures/program1/missing-import.ts');
+var ambientReference = require.resolve('./fixtures/program1/ambient-reference.ts');
 var syntaxError = require.resolve('./fixtures/program1/syntax-error.ts');
 var referenceSyntaxError = require.resolve('./fixtures/program1/ref-syntax-error.ts');
 var typeError = require.resolve('./fixtures/program1/type-error.ts');
@@ -28,140 +29,159 @@ var constEnums = require.resolve('./fixtures/program1/const-enums.ts');
 var filelist = [];
 
 function fetch(filename) {
-    //console.log("fetching " + filename);
-    filelist.push(filename);
-    var readFile = Promise.promisify(fs.readFile.bind(fs));
-    return readFile(filename, 'utf8');
+   if (filename == "ambient/ambient.d.ts") {
+      console.log('ambient');
+      return Promise.resolve("interface IAmbient { hum(); }");
+   }
+
+   //console.log("fetching " + filename);
+   filelist.push(filename);
+   var readFile = Promise.promisify(fs.readFile.bind(fs));
+   return readFile(filename, 'utf8');
 }
 
 function resolve(dep, parent) {
-    //console.log("resolving " + parent + " -> " + dep);
-    var result = "";
+   if (dep == "ambient/ambient.d.ts")
+      return Promise.resolve(dep);
 
-    if (dep[0] == '/')
-        result = dep;
-    else if (dep[0] == '.')
-        result = path.join(path.dirname(parent), dep);
-    else
-        result = require.resolve(dep);
+   //console.log("resolving " + parent + " -> " + dep);
+   var result = "";
 
-    if (path.extname(result) != '.ts')
-        result = result + ".ts";
+   if (dep[0] == '/')
+      result = dep;
+   else if (dep[0] == '.')
+      result = path.join(path.dirname(parent), dep);
+   else
+      result = require.resolve(dep);
 
-    //console.log("resolved " + parent + " -> " + result);
-    return Promise.resolve(result);
+   if (path.extname(result) != '.ts')
+      result = result + ".ts";
+
+   //console.log("resolved " + parent + " -> " + result);
+   return Promise.resolve(result);
 }
 
 describe('Incremental Compiler', function () {
 
-    var compiler;
+   var compiler;
 
-    describe('compile', function () {
-        beforeEach(function() {
-            filelist = [];
-            compiler = new Compiler(fetch, resolve);
-        });
+   describe('compile', function () {
+      beforeEach(function() {
+         filelist = [];
+         compiler = new Compiler(fetch, resolve);
+      });
 
-        it('compiles successfully', function (done) {
-            compiler.load(oneImport).then(function(txt) {
-                return compiler.compile(oneImport).then(function(output) {
-                    output.should.have.property('failure', false);
-                    output.should.have.property('errors').with.lengthOf(0);
-                    //output.should.have.property('js').with.lengthOf(0);
-                    done();
-                });
+      it('compiles successfully', function (done) {
+         compiler.load(oneImport).then(function(txt) {
+            return compiler.compile(oneImport).then(function(output) {
+               output.should.have.property('failure', false);
+               output.should.have.property('errors').with.lengthOf(0);
+               //output.should.have.property('js').with.lengthOf(0);
+               done();
+            });
+         }).catch(done);
+      });
+
+      it('compiles ambient imports', function (done) {
+         compiler.load(ambientImport).then(function(txt) {
+            return compiler.compile(ambientImport).then(function(output) {
+               output.should.have.property('failure', false);
+               output.should.have.property('errors').with.lengthOf(0);
+               //output.should.have.property('js').with.lengthOf(0);
+               done();
+            });
+         }).catch(done);
+      });
+
+      it('errors if an import is missing', function (done) {
+         compiler.load(missingImport).then(function(txt) {
+            return compiler.compile(missingImport).then(function(output) {
+               output.should.have.property('failure', 42);
+               done();
+            }, function(err) {
+               err.toString().should.be.containEql('ENOENT');
+               done();
+            });
+         }).catch(done);
+      });
+
+      it('catches type-checker errors', function (done) {
+         compiler.load(typeError).then(function(txt) {
+            return compiler.compile(typeError).then(function(output) {
+               //formatErrors(output.errors, console);
+               output.should.have.property('failure', true);
+               output.should.have.property('errors').with.lengthOf(1);
+               output.errors[0].code.should.be.equal(2322);
+               done();
+            });
+         }).catch(done);
+      });
+
+      it('catches nested type-checker errors', function (done) {
+         compiler.load(nestedTypeError).then(function(txt) {
+            compiler.compile(nestedTypeError).then(function(output) {
+               //formatErrors(output.errors, console);
+               output.should.have.property('failure', true);
+               output.should.have.property('errors').with.lengthOf(1);
+               output.errors[0].code.should.be.equal(2339);
+               done();
             }).catch(done);
-        });
+         }).catch(done);
+      });
 
-        it('compiles ambient imports', function (done) {
-            compiler.load(ambientImport).then(function(txt) {
-                return compiler.compile(ambientImport).then(function(output) {
-                    output.should.have.property('failure', false);
-                    output.should.have.property('errors').with.lengthOf(0);
-                    //output.should.have.property('js').with.lengthOf(0);
-                    done();
-                });
-            }).catch(done);
-        });
+      it('fetches all the files needed for compilation', function (done) {
+         compiler.load(nestedTypeError).then(function(txt) {
+            return compiler.compile(nestedTypeError).then(function(output) {
+               //formatErrors(output.errors, console);
+               filelist.length.should.be.equal(4);
+               done();
+            });
+         }).catch(done);
+      });
 
-        it('errors if an import is missing', function (done) {
-            compiler.load(missingImport).then(function(txt) {
-                return compiler.compile(missingImport).then(function(output) {
-                    output.should.have.property('failure', 42);
-                    done();
-                }, function(err) {
-                    err.toString().should.be.containEql('ENOENT');
-                    done();
-                });
-            }).catch(done);
-        });
+      it('catches syntax errors', function (done) {
+         compiler.load(syntaxError).then(function(txt) {
+            return compiler.compile(syntaxError).then(function(output) {
+               //formatErrors(output.errors, console);
+               output.should.have.property('failure', true);
+               output.should.have.property('errors').with.lengthOf(3);
+               done();
+            });
+         }).catch(done);
+      });
 
-        it('catches type-checker errors', function (done) {
-            compiler.load(typeError).then(function(txt) {
-                return compiler.compile(typeError).then(function(output) {
-                    //formatErrors(output.errors, console);
-                    output.should.have.property('failure', true);
-                    output.should.have.property('errors').with.lengthOf(1);
-                    output.errors[0].code.should.be.equal(2322);
-                    done();
-                });
-            }).catch(done);
-        });
+      xit('catches syntax errors in references files', function (done) {
+         compiler.load(referenceSyntaxError).then(function(txt) {
+            return compiler.compile(referenceSyntaxError).then(function(output) {
+               //formatErrors(output.errors, console);
+               output.should.have.property('failure', true);
+               output.should.have.property('errors').with.lengthOf(3);
+               done();
+            });
+         }).catch(done);
+      });
 
-        it('catches nested type-checker errors', function (done) {
-            compiler.load(nestedTypeError).then(function(txt) {
-                compiler.compile(nestedTypeError).then(function(output) {
-                    //formatErrors(output.errors, console);
-                    output.should.have.property('failure', true);
-                    output.should.have.property('errors').with.lengthOf(1);
-                    output.errors[0].code.should.be.equal(2339);
-                    done();
-                }).catch(done);
-            }).catch(done);
-        });
+      xit('handles ambient references', function (done) {
+         compiler.load(ambientReference).then(function(txt) {
+            return compiler.compile(ambientReference).then(function(output) {
+               formatErrors(output.errors, console);
+               output.should.have.property('failure', false);
+               output.should.have.property('errors').with.lengthOf(0);
+               done();
+            });
+         }).catch(done);
+      });
 
-        it('fetches all the files needed for compilation', function (done) {
-            compiler.load(nestedTypeError).then(function(txt) {
-                return compiler.compile(nestedTypeError).then(function(output) {
-                    //formatErrors(output.errors, console);
-                    filelist.length.should.be.equal(4);
-                    done();
-                });
-            }).catch(done);
-        });
-
-        it('catches syntax errors', function (done) {
-            compiler.load(syntaxError).then(function(txt) {
-                return compiler.compile(syntaxError).then(function(output) {
-                    //formatErrors(output.errors, console);
-                    output.should.have.property('failure', true);
-                    output.should.have.property('errors').with.lengthOf(3);
-                    done();
-                });
-            }).catch(done);
-        });
-
-        it('catches syntax errors in references files', function (done) {
-            compiler.load(referenceSyntaxError).then(function(txt) {
-                return compiler.compile(referenceSyntaxError).then(function(output) {
-                    formatErrors(output.errors, console);
-                    //output.should.have.property('failure', true);
-                    //output.should.have.property('errors').with.lengthOf(3);
-                    done();
-                });
-            }).catch(done);
-        });
-
-        xit('handles const enums', function (done) {
-            compiler.load(constEnums).then(function(txt) {
-                return compiler.compile(constEnums).then(function(output) {
-                    //formatErrors(output.errors, console);
-                    output.should.have.property('failure', false);
-                    output.should.have.property('errors').with.lengthOf(0);
-                    output.js.should.containEql("return 1 /* Yes */;");
-                    done();
-                });
-            }).catch(done);
-        });
-    });
+      xit('handles const enums', function (done) {
+         compiler.load(constEnums).then(function(txt) {
+            return compiler.compile(constEnums).then(function(output) {
+               //formatErrors(output.errors, console);
+               output.should.have.property('failure', false);
+               output.should.have.property('errors').with.lengthOf(0);
+               output.js.should.containEql("return 1 /* Yes */;");
+               done();
+            });
+         }).catch(done);
+      });
+   });
 });
---
+++ test/fixtures/program1/ambient-reference.ts
@@ -0,0 +1,7 @@
+/// <reference path="ambient/ambient.d.ts" />
+
+class Enya extends IAmbient {
+   hum() {
+      console.log("mmmMmmmMMmm")
+   }
+}
