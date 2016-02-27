72bd341708ec7cac9546a4628f277674cf674b4f
Frank Wallis <fwallis@outlook.com>
2016-02-27 15:15:04 +0000

    fix(angular2): fix the clear button

--- example/angular2/shims.js
+++ example/angular2/shims.js
@@ -1,4 +1,4 @@
 // Need to set global.Promise before loading zone.js for async/await to work
 global.Promise = require("babel-runtime/core-js/promise").default;
 require('reflect-metadata');
-var Zone = require('zone.js');
+require('zone.js');
