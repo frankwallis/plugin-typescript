6c6fd1cbd08ef628dbab541b268772c9a9b112d1
Frank Wallis <fwallis@outlook.com>
2016-03-08 19:37:56 +0000

    fix(hot-reload): add relativeTo setting for chokidar

--- examples/tasks/start.js
+++ examples/tasks/start.js
@@ -2,6 +2,6 @@
 	var open = require('open');
    var chokidar = require('chokidar-socket-emitter');
 	var server = hs.createServer({"root": process.cwd()});
-   chokidar({app: server.server, dir: process.cwd(), path: process.cwd()});
+   chokidar({app: server.server, relativeTo: process.cwd(), dir: process.cwd(), path: process.cwd()});
 	server.listen(8080);
 	open("http://127.0.0.1:8080");
