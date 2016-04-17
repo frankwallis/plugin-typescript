c38fbbf54ba9511b204df739122a9d89dbec4c19
Frank Wallis <fwallis@outlook.com>
2016-04-17 12:02:05 +0100

    chore(examples): fix for angular2 crypto build problem

--- examples/angular2/jspm.config.js
+++ examples/angular2/jspm.config.js
@@ -40,6 +40,8 @@ SystemJS.config({
     "buffer": "github:jspm/nodelibs-buffer@0.2.0-alpha",
     "child_process": "github:jspm/nodelibs-child_process@0.2.0-alpha",
     "clean-css": "npm:clean-css@3.4.12",
+    "constants": "github:jspm/nodelibs-constants@0.2.0-alpha",
+    "crypto": "github:jspm/nodelibs-crypto@0.2.0-alpha",
     "css": "github:frankwallis/plugin-css@master",
     "events": "github:jspm/nodelibs-events@0.2.0-alpha",
     "fs": "github:jspm/nodelibs-fs@0.2.0-alpha",
@@ -54,6 +56,7 @@ SystemJS.config({
     "reflect-metadata": "npm:reflect-metadata@0.1.3",
     "rxjs": "npm:rxjs@5.0.0-beta.2",
     "stream": "github:jspm/nodelibs-stream@0.2.0-alpha",
+    "string_decoder": "github:jspm/nodelibs-string_decoder@0.2.0-alpha",
     "systemjs-hot-reloader": "github:capaj/systemjs-hot-reloader@0.5.7",
     "text": "github:systemjs/plugin-text@0.0.2",
     "ts": "github:frankwallis/plugin-typescript@4.0.6",
@@ -64,6 +67,167 @@ SystemJS.config({
     "zone.js": "npm:zone.js@0.6.11"
   },
   packages: {
+    "github:jspm/nodelibs-crypto@0.2.0-alpha": {
+      "map": {
+        "crypto-browserify": "npm:crypto-browserify@3.11.0"
+      }
+    },
+    "github:jspm/nodelibs-string_decoder@0.2.0-alpha": {
+      "map": {
+        "string_decoder-browserify": "npm:string_decoder@0.10.31"
+      }
+    },
+    "npm:asn1.js@4.5.2": {
+      "map": {
+        "bn.js": "npm:bn.js@4.11.3",
+        "inherits": "npm:inherits@2.0.1",
+        "minimalistic-assert": "npm:minimalistic-assert@1.0.0"
+      }
+    },
+    "npm:browserify-aes@1.0.6": {
+      "map": {
+        "buffer-xor": "npm:buffer-xor@1.0.3",
+        "cipher-base": "npm:cipher-base@1.0.2",
+        "create-hash": "npm:create-hash@1.1.2",
+        "evp_bytestokey": "npm:evp_bytestokey@1.0.0",
+        "inherits": "npm:inherits@2.0.1"
+      }
+    },
+    "npm:browserify-cipher@1.0.0": {
+      "map": {
+        "browserify-aes": "npm:browserify-aes@1.0.6",
+        "browserify-des": "npm:browserify-des@1.0.0",
+        "evp_bytestokey": "npm:evp_bytestokey@1.0.0"
+      }
+    },
+    "npm:browserify-des@1.0.0": {
+      "map": {
+        "cipher-base": "npm:cipher-base@1.0.2",
+        "des.js": "npm:des.js@1.0.0",
+        "inherits": "npm:inherits@2.0.1"
+      }
+    },
+    "npm:browserify-rsa@4.0.1": {
+      "map": {
+        "bn.js": "npm:bn.js@4.11.3",
+        "randombytes": "npm:randombytes@2.0.3"
+      }
+    },
+    "npm:browserify-sign@4.0.0": {
+      "map": {
+        "bn.js": "npm:bn.js@4.11.3",
+        "browserify-rsa": "npm:browserify-rsa@4.0.1",
+        "create-hash": "npm:create-hash@1.1.2",
+        "create-hmac": "npm:create-hmac@1.1.4",
+        "elliptic": "npm:elliptic@6.2.3",
+        "inherits": "npm:inherits@2.0.1",
+        "parse-asn1": "npm:parse-asn1@5.0.0"
+      }
+    },
+    "npm:cipher-base@1.0.2": {
+      "map": {
+        "inherits": "npm:inherits@2.0.1"
+      }
+    },
+    "npm:create-ecdh@4.0.0": {
+      "map": {
+        "bn.js": "npm:bn.js@4.11.3",
+        "elliptic": "npm:elliptic@6.2.3"
+      }
+    },
+    "npm:create-hash@1.1.2": {
+      "map": {
+        "cipher-base": "npm:cipher-base@1.0.2",
+        "inherits": "npm:inherits@2.0.1",
+        "ripemd160": "npm:ripemd160@1.0.1",
+        "sha.js": "npm:sha.js@2.4.5"
+      }
+    },
+    "npm:create-hmac@1.1.4": {
+      "map": {
+        "create-hash": "npm:create-hash@1.1.2",
+        "inherits": "npm:inherits@2.0.1"
+      }
+    },
+    "npm:crypto-browserify@3.11.0": {
+      "map": {
+        "browserify-cipher": "npm:browserify-cipher@1.0.0",
+        "browserify-sign": "npm:browserify-sign@4.0.0",
+        "create-ecdh": "npm:create-ecdh@4.0.0",
+        "create-hash": "npm:create-hash@1.1.2",
+        "create-hmac": "npm:create-hmac@1.1.4",
+        "diffie-hellman": "npm:diffie-hellman@5.0.2",
+        "inherits": "npm:inherits@2.0.1",
+        "pbkdf2": "npm:pbkdf2@3.0.4",
+        "public-encrypt": "npm:public-encrypt@4.0.0",
+        "randombytes": "npm:randombytes@2.0.3"
+      }
+    },
+    "npm:des.js@1.0.0": {
+      "map": {
+        "inherits": "npm:inherits@2.0.1",
+        "minimalistic-assert": "npm:minimalistic-assert@1.0.0"
+      }
+    },
+    "npm:diffie-hellman@5.0.2": {
+      "map": {
+        "bn.js": "npm:bn.js@4.11.3",
+        "miller-rabin": "npm:miller-rabin@4.0.0",
+        "randombytes": "npm:randombytes@2.0.3"
+      }
+    },
+    "npm:elliptic@6.2.3": {
+      "map": {
+        "bn.js": "npm:bn.js@4.11.3",
+        "brorand": "npm:brorand@1.0.5",
+        "hash.js": "npm:hash.js@1.0.3",
+        "inherits": "npm:inherits@2.0.1"
+      }
+    },
+    "npm:evp_bytestokey@1.0.0": {
+      "map": {
+        "create-hash": "npm:create-hash@1.1.2"
+      }
+    },
+    "npm:hash.js@1.0.3": {
+      "map": {
+        "inherits": "npm:inherits@2.0.1"
+      }
+    },
+    "npm:miller-rabin@4.0.0": {
+      "map": {
+        "bn.js": "npm:bn.js@4.11.3",
+        "brorand": "npm:brorand@1.0.5"
+      }
+    },
+    "npm:parse-asn1@5.0.0": {
+      "map": {
+        "asn1.js": "npm:asn1.js@4.5.2",
+        "browserify-aes": "npm:browserify-aes@1.0.6",
+        "create-hash": "npm:create-hash@1.1.2",
+        "evp_bytestokey": "npm:evp_bytestokey@1.0.0",
+        "pbkdf2": "npm:pbkdf2@3.0.4"
+      }
+    },
+    "npm:pbkdf2@3.0.4": {
+      "map": {
+        "create-hmac": "npm:create-hmac@1.1.4"
+      }
+    },
+    "npm:public-encrypt@4.0.0": {
+      "map": {
+        "bn.js": "npm:bn.js@4.11.3",
+        "browserify-rsa": "npm:browserify-rsa@4.0.1",
+        "create-hash": "npm:create-hash@1.1.2",
+        "parse-asn1": "npm:parse-asn1@5.0.0",
+        "randombytes": "npm:randombytes@2.0.3"
+      }
+    },
+    "npm:sha.js@2.4.5": {
+      "map": {
+        "inherits": "npm:inherits@2.0.1"
+      }
+    },
     "github:capaj/systemjs-hot-reloader@0.5.7": {
       "map": {
         "debug": "npm:debug@2.2.0",
