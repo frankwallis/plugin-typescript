31c590d4eeff4b57910e171fcb1eef891dde4482
Brett Mayen <bmayen@midnightsnacks.net>
2016-05-30 17:33:47 -0400

    feat(gulp): add gulp build with systemjs-builder caching

---
+++ examples/angular2/gulpfile.js
@@ -0,0 +1,69 @@
+'use strict';
+
+const gulp = require('gulp');
+const jspm = require('jspm');
+const Builder = jspm.Builder;
+
+const builderOpts = {
+	baseUrl: '.',
+	transpiler: 'ts',
+	typescriptOptions: {
+		tsconfig: true,
+		module: 'system',
+		moduleResolution: 'classic',
+		target: 'es6',
+		targetLib: 'es6',
+		typeCheck: true
+	},
+
+	packages: {
+		'ts': {
+			main: 'plugin.js'
+		},
+		typescript: {
+			main: 'lib/typescript.js',
+			meta: {
+				'lib/typescript.js': {
+					exports: 'ts'
+				}
+			}
+		}
+	}
+};
+
+const buildStaticOpts = {
+	minify: false,
+	mangle: false,
+	rollup: false,
+	sourceMaps: false,
+	format: 'umd'
+};
+
+var buildCache;
+
+gulp.task('default', function(cb) {
+	// create builder instance from JSPM-generated config
+	var builder = new Builder(builderOpts.baseUrl, './jspm.config.js');
+	builder.config(builderOpts); // set fe-build opts
+
+	if (buildCache) {
+		builder.setCache(buildCache);
+	}
+
+	builder
+		.buildStatic('src', 'build.js', buildStaticOpts)
+		.then(()=> {
+			buildCache = builder.getCache();
+		})
+		.catch(err => {
+			console.error('SystemJS Builder error:', err);
+
+			builder.reset();
+		}).finally(()=> {
+			cb();
+		});
+});
+
+gulp.task('watch', function() {
+	return gulp.watch('**/*.ts', ['default']);
+});
