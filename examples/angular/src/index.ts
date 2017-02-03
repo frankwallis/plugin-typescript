c15a513f5362c66a3cf62d4cbfae39cf8b59a791
Frank Wallis <fwallis@outlook.com>
2017-02-03 19:07:48 +0000

    chore(examples): update examples to latest

--- examples/angular/src/index.ts
+++ examples/angular/src/index.ts
@@ -1,11 +1,11 @@
-import {module} from "angular";
+import angular from "angular";
 import "./index.css";
 
 import exampleTemplate from './example-template.html';
 import {ExampleService} from './example-service';
 import {ExampleController} from './example-controller';
 
-export var Module = module("example", []);
+export var Module = angular.module("example", []);
 
 Module.service("exampleService", ExampleService);
 
